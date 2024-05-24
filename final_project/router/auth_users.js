ee929f6222e324a1c5d626c4e9b3558db8b1f40b
thomastepi <tepexminem2013@gmail.com>
2024-05-24 04:29:46 -0400

    task 7-8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,62 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
+const e = require("express");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (name, pass) => {
+  const reg_user = users.filter((user) => {
+    return user.username === name && user.password === pass;
+  });
+  return reg_user.length > 0;
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
+  if (authenticatedUser(username, password)) {
+    const accessToken = jwt.sign({ username: username }, "access", {
+      expiresIn: "1h",
+    });
+    req.session.authorization = { accessToken };
+    return res.status(200).json({ message: "User logged in successfully" });
+  } else {
+    return res.status(403).json({ message: "Invalid username or password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const { review } = req.query;
+  const token = req.session.authorization["accessToken"];
+  jwt.verify(token, "access", (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "User not authenticated" });
+    }
+  });
+  const { username } = jwt.decode(token);
+  if (!isbn || !review) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
+  if (books[isbn]) {
+    books[isbn].reviews = { ...books[isbn].review, [username]: review };
+    return res
+      .status(200)
+      .json({
+        message: "Review added successfully",
+        review: books[isbn].reviews,
+      });
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.authenticated = regd_users;
